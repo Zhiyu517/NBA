04d2332acbc29a046649823e93e86b27613db7bf
Zhiyu517 <wangzhiyu0517@gmail.com>
2018-12-31 11:54:27 -0800

    profile

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import {Profile} from './Profile'
+import {ShotChart} from "./ShotChart"
+import nba from 'nba'
+
 export class Main extends React.Component {
+    state = {
+        playerId: nba.findPlayer('Stephen Curry').playerId
+    }
+
+    componentDidMount() {
+        nba.stats.playerInfo({
+            PlayerID: this.state.playerId
+        }).then(
+            (response) => {
+                console.log(response);
+        }
+        )
+    }
     render() {
         return (
-            <div className = "main">Main</div>
+            <div className = "main">
+                <Profile/>
+                <ShotChart playerId={this.state.playerId}/>
+            </div>
         );
     }
 }
